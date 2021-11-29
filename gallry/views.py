62ed25b5fb89978e19569f48e76cd0d33a121c24
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-29 12:18:56 +0300

    updated views

--- gallry/views.py
+++ gallry/views.py
@@ -5,7 +5,7 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'index.html')
 def news_of_day(request):
     date = dt.date.today()
 
