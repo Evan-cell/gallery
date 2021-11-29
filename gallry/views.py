0e967bafc3dd85d86bcf716389a56e13ee551f2c
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-29 12:39:38 +0300

    date filter convert

--- gallry/views.py
+++ gallry/views.py
@@ -10,25 +10,16 @@ def news_of_day(request):
     date = dt.date.today()
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
+   
     html = f'''
         <html>
             <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+                <h1>News for  {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
     return HttpResponse(html)
-def convert_dates(dates):
-    
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
 def past_days_news(request,past_date):
     
     try:
@@ -38,11 +29,11 @@ def past_days_news(request,past_date):
         # Raise 404 error when ValueError is thrown
         raise Http404()
 
-    day = convert_dates(date)
+  
     html = f'''
         <html>
             <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+                <h1>News for {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
