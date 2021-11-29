d99bd1149d637619d361c6099023e888a37eb902
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-29 12:14:37 +0300

    template settings

--- tribgallory/settings.py
+++ tribgallory/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallry.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
